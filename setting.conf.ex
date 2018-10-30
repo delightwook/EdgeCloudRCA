ecd0811e8b2796b3f320b31d69395298ed8c2e51
delightwook91 <delightwook@naver.com>
2018-10-30 15:41:03 +0900

    Add conf file

---
+++ setting.conf.ex
@@ -0,0 +1,12 @@
+# PATH : /opt/stack/ecsetting/setting.conf
+
+[Default]
+eclist = MECsite1
+
+[MECsite1]
+auth_url = http://192.168.11.101/identity
+user_name = admin
+password = root
+project_name = admin
+project_domain_name = default
+user_domain_name = default
